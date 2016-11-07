ed5cbfacb3c604be2cfc799a7c8e3c38be10af1c
Robert Jesionek <yesionuk@gmail.com>
2016-11-07 19:48:37 +0100

    altered PassOfferResult class

--- src/main/java/com/cleeng/api/domain/PassOfferResult.java
+++ src/main/java/com/cleeng/api/domain/PassOfferResult.java
@@ -1,9 +1,11 @@
 package com.cleeng.api.domain;
 
+import java.util.List;
+
 /**
  * Created by Jesion on 2016-11-01.
  */
-public class PassOfferResult extends PassOfferData {
+public class PassOfferResult extends BaseOfferData {
 
     public String id;
     public String publisherEmail;
@@ -16,4 +18,7 @@ public class PassOfferResult extends PassOfferData {
     public int socialCommissionRate;
     public int averageRating;
     public String contentType;
+    public List<String> accessToTags;
+    public String period;
+    public long expiresAt;
 }
