ef7ea8098c0ec561c712ca18005fe6af902e6b7b
Robert Jesionek <yesionuk@gmail.com>
2016-11-07 19:55:43 +0100

    altered Criteria class to include associateEmail field

--- src/main/java/com/cleeng/api/domain/Criteria.java
+++ src/main/java/com/cleeng/api/domain/Criteria.java
@@ -9,11 +9,20 @@ public class Criteria implements Serializable {
 
     public boolean active;
 
+    public String associateEmail;
+
     public Criteria() {
 
     }
 
     public Criteria(boolean active) {
+
         this.active = active;
+    }
+
+    public Criteria(boolean active, String associateEmail) {
+
+        this.active = active;
+        this.associateEmail = associateEmail;
     }
 }
