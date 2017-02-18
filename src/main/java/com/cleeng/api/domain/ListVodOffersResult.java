8d3169cc33779d01575317eeea8c704e2cf17e12
Robert Jesionek <yesionuk@gmail.com>
2017-02-18 08:59:55 +0100

    constructing proper objects on listVodOffers and listVodOffersAsync

--- src/main/java/com/cleeng/api/domain/ListVodOffersResult.java
+++ src/main/java/com/cleeng/api/domain/ListVodOffersResult.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class ListVodOffersResult implements Serializable {
 
-    public List<Vod> items;
+    public List<VodOffer> items;
 
     public int totalItemCount;
 }
