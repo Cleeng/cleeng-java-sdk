e66eba5745c30d5fa6ac4bf6d122e7c7533a1650
Robert Jesionek <yesionuk@gmail.com>
2017-03-16 21:23:56 +0100

    completed mapper

--- src/main/java/com/cleeng/api/ResponseMapper.java
+++ src/main/java/com/cleeng/api/ResponseMapper.java
@@ -18,7 +18,38 @@ public class ResponseMapper {
     }
 
     private void build() {
-        this.map.put("createSubscriptionOffer", "com.cleeng.api.domain.OfferResponse");
-        this.map.put("listSubscriptionOffers", "com.cleeng.api.domain.ListSubscriptionOffersResponse");
+        String domainPath = "com.cleeng.api.domain.";
+        this.map.put("createSubscriptionOffer", domainPath + "OfferResponse");
+        this.map.put("listSubscriptionOffers", domainPath + "ListSubscriptionOffersResponse");
+        this.map.put("updateSubscriptionOffer", domainPath + "OfferResponse");
+        this.map.put("createSingleOffer", domainPath + "SingleOfferResponse");
+        this.map.put("updateSingleOffer", domainPath + "SingleOfferResponse");
+        this.map.put("createEventOffer", domainPath + "EventOfferResponse");
+        this.map.put("updateEventOffer", domainPath + "EventOfferResponse");
+        this.map.put("createRentalOffer", domainPath + "RentalOfferResponse");
+        this.map.put("updateRentalOffer", domainPath + "RentalOfferResponse");
+        this.map.put("createPassOffer", domainPath + "PassOfferResponse");
+        this.map.put("updatePassOffer", domainPath + "OfferResponse");
+        this.map.put("listSubscriptionOffers", domainPath + "ListSubscriptionOffersResponse");
+        this.map.put("listSingleOffers", domainPath + "ListSingleOffersResponse");
+        this.map.put("listVodOffers", domainPath + "ListVodOffersResponse");
+        this.map.put("listPassOffers", domainPath + "ListPassOffersResponse");
+        this.map.put("prepareRemoteAuth", domainPath + "PrepareRemoteAuthResponse");
+        this.map.put("generateCustomerToken", domainPath + "TokenResponse");
+        this.map.put("requestPasswordReset", domainPath + "BooleanResponse");
+        this.map.put("updateCustomerPassword", domainPath + "BooleanResponse");
+        this.map.put("generateCustomerTokenFromFacebook", domainPath + "TokenResponse");
+        this.map.put("generateCustomerTokenFromPassword", domainPath + "TokenResponse");
+        this.map.put("getAccessStatus", domainPath + "GetAccessStatusResponse");
+        this.map.put("getAccessibleTags", domainPath + "GetAccessibleTagsResponse");
+        this.map.put("getCustomer", domainPath + "GetCustomerResponse");
+        this.map.put("createVodOffer", domainPath + "VodOfferResponse");
+        this.map.put("getVodOffer", domainPath + "VodOfferResponse");
+        this.map.put("updateVodOffer", domainPath + "VodOfferResponse");
+        this.map.put("generateCheckoutUrl", domainPath + "UrlResponse");
+        this.map.put("registerCustomer", domainPath + "TokenResponse");
+        this.map.put("generateMyAccountUrl", domainPath + "UrlResponse");
+        this.map.put("listOfferIdsByVideoId", domainPath + "ListOfferIdsByVideoIdResponse");
+        this.map.put("getAccessStatusForDevice", domainPath + "GetAccessStatusForDeviceResponse");
     }
 }
