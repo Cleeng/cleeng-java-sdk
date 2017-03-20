9c982b5d4986aa3ecb12c26d3cd5fa778a24e2b0
Robert Jesionek <yesionuk@gmail.com>
2017-03-20 16:32:40 +0100

    configurable platform urls

--- src/main/java/com/cleeng/api/CleengFactory.java
+++ src/main/java/com/cleeng/api/CleengFactory.java
@@ -7,17 +7,15 @@ package com.cleeng.api;
  */
 public class CleengFactory {
 
-	final static String SANDBOX_URL = "https://sandbox.cleeng.com/api/3.0/json-rpc";
-	final static String PRODUCTION_URL = "https://cleeng.com/api/3.0/json-rpc";
-
 	public static Cleeng createSandboxApi(String publisherToken) {
-		return new CleengBuilder(SANDBOX_URL, publisherToken)
+		return new CleengBuilder(publisherToken)
 				.setUseNonBlockingMode(1)
+				.setUseSandbox(true)
 				.build();
 	}
 
 	public static Cleeng createProductionApi(String publisherToken) {
-		return new CleengBuilder(PRODUCTION_URL, publisherToken)
+		return new CleengBuilder(publisherToken)
 				.setUseNonBlockingMode(1)
 				.build();
 	}
