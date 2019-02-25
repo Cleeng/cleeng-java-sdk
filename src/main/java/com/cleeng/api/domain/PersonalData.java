5ca2495b9f0a39dcf58151ce1d0e1b637f9f92d8
Robert Jesionek <yesionuk@gmail.com>
2019-02-25 19:40:34 +0100

    added new fields to personal data object

--- src/main/java/com/cleeng/api/domain/PersonalData.java
+++ src/main/java/com/cleeng/api/domain/PersonalData.java
@@ -1,20 +1,53 @@
 package com.cleeng.api.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class PersonalData implements Serializable {
 
     public String firstName;
+    public String lastName;
     public String address;
+    public String address2;
+    public String city;
+    public String postCode;
     public String country;
+    public String email;
+    public String birthDate;
+    public String companyName;
+    public String phoneNumber;
+    public Integer broadcasterId;
+    public ArrayList<Answer> customAnswers;
 
     public PersonalData() {
 
     }
 
-    public PersonalData(String firstName, String address, String country) {
+    public PersonalData(String firstName,
+                        String lastName,
+                        String address,
+                        String address2,
+                        String city,
+                        String postCode,
+                        String country,
+                        String email,
+                        String birthDate,
+                        String companyName,
+                        String phoneNumber,
+                        Integer broadcasterId,
+                        ArrayList<Answer> customAnswers) {
         this.firstName = firstName;
+        this.lastName = lastName;
         this.address = address;
+        this.address2 = address2;
+        this.city = city;
+        this.postCode = postCode;
         this.country = country;
+        this.email = email;
+        this.birthDate = birthDate;
+        this.companyName = companyName;
+        this.phoneNumber = phoneNumber;
+        this.broadcasterId = broadcasterId;
+        this.customAnswers = customAnswers;
     }
 }
